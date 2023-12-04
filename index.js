3e61581b83576a5fb6e04f7f6b8c80e2875691b4
Sumaiya Sinja <sumiyasinja@gmail.com>
2023-12-04 17:37:38 +0600

    add payement intent

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(
 app.use(express.json());
 app.use(cookieParser());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctrkbrk.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -35,213 +34,412 @@ async function run() {
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
-      // database collections
-    const articlesCollection = client.db("theDailyPulseNews").collection("articles");
+    // database collections
+    const articlesCollection = client
+      .db("theDailyPulseNews")
+      .collection("articles");
     const userCollection = client.db("theDailyPulseNews").collection("users");
-    const publisherCollection = client.db("theDailyPulseNews").collection("publishers");
+    const publisherCollection = client
+      .db("theDailyPulseNews")
+      .collection("publishers");
 
     // middlewares
     const verifyToken = (req, res, next) => {
+      // console.log('inside verify token', req.headers.authorization);
       if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'unauthorized access' });
+        return res.status(401).send({ message: "unauthorized access" });
       }
-      const token = req.headers.authorization.split(' ')[1];
+      const token = req.headers.authorization.split(" ")[1];
       jwt.verify(token, process.env.SECRET, (err, decoded) => {
         if (err) {
-          return res.status(401).send({ message: 'unauthorized access' })
+          return res.status(401).send({ message: "unauthorized access" });
         }
         req.decoded = decoded;
-        log('decoded', decoded);
         next();
-      })
-    }
-    
+      });
+    };
+
     // use verify admin after verifyToken
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
       const user = await userCollection.findOne(query);
-      const isAdmin = user?.role === 'admin';
+      const isAdmin = user?.role === "admin";
       if (!isAdmin) {
-        return res.status(403).send({ message: 'forbidden access' });
+        return res.status(403).send({ message: "forbidden access" });
       }
       next();
-    }
+    };
+    const verifyPremium = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isPremiumTaken = user?.premiumTaken === "true";
+      if (!isPremiumTaken) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+     // payment intent
+app.post('/create-payment-intent', async (req, res) => {
+  const { price } = req.body;
+  const amount = parseInt(price * 100);
+  console.log(amount, 'amount inside the intent');
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount,
+    currency: 'usd',
+    payment_method_types: ['card'],
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
 
     // artciles api
-    app.post("/articles", async (req, res) => {
+    // get all articles
+    app.get("/articles", verifyToken, verifyAdmin,async (req, res) => {
       try {
-         const article = req.body;
-         const result = await articlesCollection.insertOne(article);
-         res.send(result);
+        const result = await articlesCollection.find().toArray();
+        res.send(result);
       } catch (error) {
-         console.error(error);
-         res.status(500).send({ success: false, message: "Internal server error" });
+        console.error(error);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
       }
-   });
-   
-   app.get("/articles", async (req, res) => {
-    try {
-      // filter  status==="approved"
-      const result = await articlesCollection.find().toArray();
-      res.send(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send({ success: false, message: "Internal server error" });
-    }
-  });
-  
-    app.get("/articles/:id", async(req, res) => {
+    });
+    app.get("/article-deatials/:id", async (req, res) => {
       const id = req.params.id;
+      console.log("prob",id);
       const query = { _id: new ObjectId(id) };
-      const result =await articlesCollection.findOne(query);
+      const result = await articlesCollection.findOne(query);
       res.send(result);
     });
-    app.put("/articles/:id", async(req, res) => {
+    app.post("/articles", async (req, res) => {
+      try {
+        const article = req.body;
+        const result = await articlesCollection.insertOne(article);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res
+          .status(500)
+          .send({ success: false, message: "Internal server error" });
+      }
+    });
+
+    app.get("/premium-articles", async (req, res) => {
+      try {
+        const result = await articlesCollection
+          .find({ status: "approved", isPremium: "true" })
+          .toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+          error: error.message,
+        });
+      }
+    });
+    app.get("/articles-approved", async (req, res) => {
+      try {
+        // Filter articles with status "approved"
+        const result = await articlesCollection
+          .find({ status: "approved" })
+          .toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+          error: error.message,
+        });
+      }
+    });
+    app.get("/articles/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      const query = { "author.email": email };
+      try {
+        const result = await articlesCollection.find(query).toArray();
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+          error: error.message,
+        });
+      }
+    });
+
+    app.put(
+      "/articles/change-status-decline/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const article = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            status: "declined",
+            feedback: article.feedback,
+          },
+        };
+        const result = await articlesCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      }
+    );
+    app.put(
+      "/articles/change-status-approve/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const article = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            status: "approved",
+          },
+        };
+        const result = await articlesCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      }
+    );
+    app.put(
+      "/articles/make-premium/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const article = req.body;
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            isPremium: "true",
+          },
+        };
+        const result = await articlesCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+        res.send(result);
+      }
+    );
+    app.put("/articles/update-views/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const article = req.body;
       const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      // todo: add validation, set feilds data individually
       const updateDoc = {
         $set: {
           views: article.views,
-          
         },
       };
-      const result =await articlesCollection.updateOne(filter, updateDoc, options);
+      const result = await articlesCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
-    app.delete("/articles/:id", async(req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result =await articlesCollection.deleteOne(query);
-      res.send(result);
+    app.put("/articles/:id", verifyToken, async (req, res) => {
+      try {
+        const id = req.params.id;
+        const article = req.body;
+
+        console.log(article);
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            title: article.title,
+            description: article?.description,
+            image: article?.image,
+            publisher: article?.publisher,
+            tags: article?.tags,
+          },
+        };
+
+        const result = await articlesCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
+
+        res.send(result);
+      } catch (error) {
+        console.error("Error updating article:", error);
+        res.status(500).json({
+          success: false,
+          message: "Internal server error",
+          error: error.message,
+        });
+      }
     });
 
     // user api
-    app.post('/users', async (req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
-      const query = { email: user.email }
+      const query = { email: user.email };
       const existingUser = await userCollection.findOne(query);
       if (existingUser) {
-        res.send({ message: 'user already exists', insertedId: null });
-        return; 
+        res.send({ message: "user already exists", insertedId: null });
+        return;
       }
-      
+
       try {
         const result = await userCollection.insertOne(user);
         res.send(result);
       } catch (error) {
-        console.error('Error inserting user:', error);
+        console.error("Error inserting user:", error);
         res.status(500).send({ error: error.message });
       }
-      
     });
-    app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+    app.get("/users", async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
-    app.get("/users/:id", verifyToken, async(req, res) => {
+
+    app.get("/users/:id", verifyToken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.findOne(query);
       res.send(result);
     });
+    app.put("/users/update-role/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
 
-    app.put(`/users/update/:email`,verifyToken, async (req, res) => {
+    app.put(`/users/update/:email`, async (req, res) => {
       const userEmail = req.params.email;
       const user = req.body;
       console.log("Received request:", req.body);
 
-      // if (userEmail !== req.decoded.email) {
-      //   return res.status(403).send({ message: 'forbidden access' })
-      // }
-      const filter = { email: userEmail } 
-      
+      const filter = { email: userEmail };
+
       const options = { upsert: true };
       const updateDoc = {
         $set: {
           name: user.name,
           photoURL: user.photoURL,
-        }
+        },
       };
-      
-    
+
       try {
-        const result = await userCollection.updateOne(filter, updateDoc, options);
+        const result = await userCollection.updateOne(
+          filter,
+          updateDoc,
+          options
+        );
         console.log("Result:", result);
         res.send(result);
       } catch (error) {
         console.error("Error updating user:", error);
         res.status(500).send({ error: error.message });
       }
-      
     });
-    
-    app.get('/users/admin/:email', verifyToken, async (req, res) => {
-      const email = req.params.email;
 
-      if (email !== req.decoded.email) {
-        return res.status(403).send({ message: 'forbidden access' })
-      }
+    app.get(
+      "/users/admin/:email",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
 
-      const query = { email: email };
-      const user = await userCollection.findOne(query);
-      let admin = false;
-      if (user) {
-        admin = user?.role === 'admin';
-      }
-      res.send({ admin });
-    })
-
-    app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const updatedDoc = {
-        $set: {
-          role: 'admin'
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "forbidden access" });
         }
+
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === "admin";
+        }
+        res.send({ admin });
       }
-      const result = await userCollection.updateOne(filter, updatedDoc);
-      res.send(result);
-    })
+    );
+
+    app.patch(
+      "/users/admin/:id",
+      verifyToken,
+      verifyAdmin,
+      async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = {
+          $set: {
+            role: "admin",
+          },
+        };
+        const result = await userCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      }
+    );
 
     // publisher api
-    app.post("/publishers", async(req, res) => {
+    app.post("/publishers", async (req, res) => {
       const user = req.body;
-      const result =await publisherCollection.insertOne(user);
+      const result = await publisherCollection.insertOne(user);
       res.send(result);
     });
-    app.get("/publishers/:id", async(req, res) => {
+    app.get("/publishers/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result =await publisherCollection.findOne(query);
+      const result = await publisherCollection.findOne(query);
       res.send(result);
     });
-    app.get("/publishers", async(req, res) => {
+    app.get("/publishers", async (req, res) => {
       const result = await publisherCollection.find().toArray();
       res.send(result);
     });
-    app.delete("/publishers/:id", async(req, res) => {
+    app.delete("/publishers/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await publisherCollection.deleteOne(query);
       res.send(result);
     });
     // jwt api
- // jwt related api
-      app.post('/jwt', async (req, res) => {
-        const user = req.body;
-        const token = jwt.sign(user, process.env.SECRET, { expiresIn: '1h' });
-        res.send({ token });
-      })
+    // jwt related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET, { expiresIn: "1h" });
+      res.send({ token });
+    });
 
     app.delete("/jwt", (req, res) => {
       res.clearCookie("token");
       res.send({ success: true });
-
-    })
+    });
   } finally {
   }
 }
